c6ddd6e89f62b2c449aaa60183826640bbeaded4
Kristen Zirkler <bazirkler@gmail.com>
2015-04-17 13:59:55 -0700

    Home page updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
 // Main JS File Unminified 
+
+$(function(){
+
+	// Function to check hero placement
+	function imageFade(){
+	// change grayscale on scroll
+		var y = $(document).scrollTop();
+		//var t = $('.hero').offset().top - 100;
+		var t = 1;
+
+		if (y > t) {
+		    $('.home-intro').addClass('fadetocolor');
+		} else {
+			$('.home-intro').removeClass('fadetocolor');
+		}
+
+	}
+	// Function to check hero placement
+	function navFade(){
+	// change grayscale on scroll
+		var y = $(document).scrollTop();
+		//var t = $('.hero').offset().top - 100;
+		var t = 50;
+
+		if (y > t) {
+		    $('.site-tag').addClass('show');
+		} else {
+		    //$('.site-tag').removeClass('show');
+		}
+
+	}
+
+	// Function to check hero placement
+	function startGrowAni(){
+	// change grayscale on scroll
+		var y = $(document).scrollTop();
+		var t = $('.start-grow').offset().top - 100;
+
+		if (y > t) {
+		    $('.start-grow').addClass('animate');
+		} else {
+			//$('.start-grow').removeClass('animate');
+		}
+	}
+
+	// Hero update on scroll
+	$(document).scroll(function () {
+		navFade();
+		imageFade();
+		startGrowAni();
+	});
+
+});
