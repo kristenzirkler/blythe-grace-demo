85c158799c557ce401d5a96b87ec4e2427d6d704
Kristen Zirkler <bazirkler@gmail.com>
2015-05-27 10:49:17 -0700

    js error fixed. map updated.

--- js/main.js
+++ js/main.js
@@ -115,92 +115,94 @@ $(function(){
 
 
 // Google Map -----------------------
-function map_recenter(latlng,offsetx,offsety) {
-    var point1 = map.getProjection().fromLatLngToPoint(
-        (latlng instanceof google.maps.LatLng) ? latlng : map.getCenter()
-    );
-    var point2 = new google.maps.Point(
-        ( (typeof(offsetx) === 'number' ? offsetx : 0) / Math.pow(2, map.getZoom()) ) || 0,
-        ( (typeof(offsety) === 'number' ? offsety : 0) / Math.pow(2, map.getZoom()) ) || 0
-    );  
-    map.setCenter(map.getProjection().fromPointToLatLng(new google.maps.Point(
-        point1.x - point2.x,
-        point1.y + point2.y
-    )));
-}
+if ($('body').hasClass('home') || $('body').hasClass('contact')) {
+    function map_recenter(latlng,offsetx,offsety) {
+        var point1 = map.getProjection().fromLatLngToPoint(
+            (latlng instanceof google.maps.LatLng) ? latlng : map.getCenter()
+        );
+        var point2 = new google.maps.Point(
+            ( (typeof(offsetx) === 'number' ? offsetx : 0) / Math.pow(2, map.getZoom()) ) || 0,
+            ( (typeof(offsety) === 'number' ? offsety : 0) / Math.pow(2, map.getZoom()) ) || 0
+        );  
+        map.setCenter(map.getProjection().fromPointToLatLng(new google.maps.Point(
+            point1.x - point2.x,
+            point1.y + point2.y
+        )));
+    }
 
-var map,
-	targetLocation = new google.maps.LatLng(33.48067, -111.94530),
-	mapStyles = [
-		{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},
-		{"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},
-		{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},
-		//{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},
-		{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-		{"stylers":[{"hue":"#00aaff"},{"saturation":-80},{"gamma":1.75},{"lightness":25}]},
-		{"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"lightness":10}]},
-		{"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}],
-    mapHoverStyles = [
-        {"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},
-        {"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},
-        {"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},
-        //{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},
-        {"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-        //{"stylers":[{"hue":"#00aaff"},{"saturation":-80},{"gamma":1.75},{"lightness":25}]},
-        //{"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"lightness":10}]},
-        {"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}],
-	mapOptions = {
-		// How zoomed in you want the map to start at (always required)
-		zoom: 13,
-		disableDefaultUI: true,
-		scrollwheel: false,
-		draggable: false,
-		panControl: false,
+    var map,
+    	targetLocation = new google.maps.LatLng(33.48067, -111.94530),
+    	mapStyles = [
+    		{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},
+    		{"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},
+    		{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},
+    		//{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},
+    		{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
+    		{"stylers":[{"hue":"#00aaff"},{"saturation":-80},{"gamma":1.75},{"lightness":25}]},
+    		{"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"lightness":10}]},
+    		{"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}],
+        mapHoverStyles = [
+            {"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},
+            {"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},
+            {"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},
+            //{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},
+            {"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
+            //{"stylers":[{"hue":"#00aaff"},{"saturation":-80},{"gamma":1.75},{"lightness":25}]},
+            //{"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"lightness":10}]},
+            {"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}],
+    	mapOptions = {
+    		// How zoomed in you want the map to start at (always required)
+    		zoom: 13,
+    		disableDefaultUI: true,
+    		scrollwheel: false,
+    		draggable: false,
+    		panControl: false,
 
-		// The latitude and longitude to center the map (always required)
-		center: targetLocation, // BlytheGraceOffice
+    		// The latitude and longitude to center the map (always required)
+    		center: targetLocation, // BlytheGraceOffice
 
-		// How you would like to style the map. 
-		// This is where you would paste any style found on Snazzy Maps.
-		styles: mapStyles
-	};
+    		// How you would like to style the map. 
+    		// This is where you would paste any style found on Snazzy Maps.
+    		styles: mapStyles
+    	};
 
 
 
-function initialize() {
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-}
+    function initialize() {
+      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    }
 
-function newMapOptions() {
-    map.set('zoom', 15);
-    map.set('styles', mapHoverStyles);
-    map_recenter(targetLocation,150,0);
-}
-function addMarkerToMap(lat, long){
-    var mapMarkerIcon = {
-      url: 'img/marker-icon-2.png',
-      size: new google.maps.Size(80, 80),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(40, 40),
-      scaledSize: new google.maps.Size(40, 40)
-    };
-    var myLatLng = new google.maps.LatLng(lat, long);
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-		icon: mapMarkerIcon
+    function newMapOptions() {
+        map.set('zoom', 15);
+        map.set('styles', mapHoverStyles);
+        map_recenter(targetLocation,150,0);
+    }
+    function addMarkerToMap(lat, long){
+        var mapMarkerIcon = {
+          url: 'img/marker-icon-2.png',
+          size: new google.maps.Size(80, 80),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(40, 40),
+          scaledSize: new google.maps.Size(40, 40)
+        };
+        var myLatLng = new google.maps.LatLng(lat, long);
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+    		icon: mapMarkerIcon
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    $('.map-slide').hover(function () {
+      newMapOptions();
+      $(this).addClass('map-open');
+      addMarkerToMap(33.48067, -111.94530);
+    });
+    $('.map-slide').click(function () {
+    	window.open('https://maps.google.com?daddr=6350+East+Thomas+Road+Suite+330+Scottsdale+Arizona+85251');
     });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
-
-$('.map-slide').hover(function () {
-  newMapOptions();
-  $(this).addClass('map-open');
-  addMarkerToMap(33.48067, -111.94530);
-});
-$('.map-slide').click(function () {
-	window.open('https://maps.google.com?daddr=6350+East+Thomas+Road+Suite+330+Scottsdale+Arizona+85251');
-});
 
 
 
